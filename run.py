46ba14eabe723027b3fc29ccd1900002150a99a1
KhanWarden <baryrhan2004@gmail.com>
2024-10-29 02:14:13 +0500

    Imported Redis in run file

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import asyncio
+import redis
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.redis import RedisStorage
+
 from app.config import API_TOKEN
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+storage = RedisStorage(redis_client)
 
 
 async def main():
