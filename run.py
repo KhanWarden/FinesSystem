323147b6c17588cde6b77a209d0a2ee0cb19c74e
KhanWarden <baryrhan2004@gmail.com>
2024-10-29 23:23:14 +0500

    Parse mode of Bot is HTML now

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import asyncio
 import redis
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
-
+from app.handlers import calculator, admin
 from app.config import API_TOKEN
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 storage = RedisStorage(redis_client)
+bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+dp.include_router(calculator.router)
 
 
 async def main():
