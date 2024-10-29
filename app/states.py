d9236fdd233408a98e8e0ba120fa9559a3c36dd2
KhanWarden <baryrhan2004@gmail.com>
2024-10-29 23:23:35 +0500

    Added new states for calculator.py

--- app/states.py
+++ app/states.py
@@ -8,4 +8,6 @@ class AdminStates(StatesGroup):
 class CalculatorStates(StatesGroup):
     waiting_for_guests_amount = State()
     waiting_for_duration = State()
-    waiting_for_discount = State()
+    waiting_for_range_value = State()
+    waiting_for_percentage_discount = State()
+    waiting_for_numerical_discount = State()
