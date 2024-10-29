cff9109fd5f6cbeca2e1067602b398da4869f21a
KhanWarden <baryrhan2004@gmail.com>
2024-10-29 23:24:04 +0500

    Moved prices to calculator_methods.py

--- app/keyboards/inline_kbs.py
+++ app/keyboards/inline_kbs.py
@@ -1,26 +1,4 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-prices = {
-    "1_hour": {
-        "fix": 100000,
-        "16-25": 6000,
-        "26-34": 5500,
-        "35": 4500
-    },
-    "1.5_hours": {
-        "fix": 150000,
-        "16-25": 9000,
-        "26-34": 8250,
-        "35": 6750
-    },
-    "2_hours": {
-        "fix": 200000,
-        "16-25": 12000,
-        "26-34": 11000,
-        "35": 9000
-    }
-}
 
 
 def game_counter(buttons_data):
