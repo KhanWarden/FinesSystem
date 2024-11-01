04ada55fc0a66d592d86d6dd3a2f4cf79b63f7a5
KhanWarden <baryrhan2004@gmail.com>
2024-11-02 02:30:49 +0500

    Include another router

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
-from app.handlers import calculator, admin
+from app.handlers import calculator, admin, main
 from app.config import API_TOKEN
 
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 storage = RedisStorage(redis_client)
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
-dp.include_router(calculator.router)
+dp.include_routers(calculator.router, main.router)
 
 
 async def main():
