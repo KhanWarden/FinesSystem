4ec5fdccf7990ab7f7c493af9952ddb45f6a4848
KhanWarden <baryrhan2004@gmail.com>
2024-11-03 03:08:58 +0500

    Include 1 router in run file instead of using it individually

--- run.py
+++ run.py
@@ -4,14 +4,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import RedisStorage
-from app.handlers import calculator, admin, main
+from app.handlers import main_router
 from app.config import API_TOKEN
 
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 storage = RedisStorage(redis_client)
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
-dp.include_routers(calculator.router, main.router)
+dp.include_router(main_router)
 
 
 async def main():
