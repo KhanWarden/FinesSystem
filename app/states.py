9b0b52c977cb131e8ad1fdf2b867c89ec46d6ba2
KhanWarden <baryrhan2004@gmail.com>
2024-11-03 03:07:03 +0500

    Added new states

--- app/states.py
+++ app/states.py
@@ -2,7 +2,10 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class AdminStates(StatesGroup):
+    wait_for_user_id = State()
     wait_for_admin_id = State()
+    wait_for_new_employee_id = State()
+    wait_for_new_employee_name = State()
 
 
 class CalculatorStates(StatesGroup):
